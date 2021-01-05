c60365f4a1cb6b17c93c97d242e4aa5ea9fd1d2c
Yu Gao <gaoyububest@gmail.com>
2021-01-05 16:16:23 +0000

    Make one-to-many relations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Justin", last_name: "Trudeau", hourly_rate: 80)
+@store1.employees.create(first_name: "Vladimir", last_name: "Putin", hourly_rate: 80)
+@store2.employees.create(first_name: "Don", last_name: "Trump", hourly_rate: 30)
+@store2.employees.create(first_name: "Joe", last_name: "Biden", hourly_rate: 80)
+@store2.employees.create(first_name: "Jinping", last_name: "Xi", hourly_rate: 60)
