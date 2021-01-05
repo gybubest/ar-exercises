6d1f85c5759da68e33519fa48b2d6ed4f7c2df67
Yu Gao <gaoyububest@gmail.com>
2021-01-05 15:51:23 +0000

    Make calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts @sum_of_all = Store.sum("annual_revenue")
+puts @average_annual_revenue = @sum_of_all / Store.count
+puts Store.where(["annual_revenue >= ?", 1000000]).count
